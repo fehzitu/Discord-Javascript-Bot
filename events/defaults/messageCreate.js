1050adb4c639df1f212fdb205284094f93e4b94f
fehzitu <130912488+fehzitu@users.noreply.github.com>
2025-11-10 21:59:44 -0300

    Fix channel name assignment in messageCreate.js

--- events/defaults/messageCreate.js
+++ events/defaults/messageCreate.js
@@ -4,7 +4,7 @@ module.exports = {
   name: Discord.Events.MessageCreate,
   async execute(message) {
     let guildName = message.guild? message.guild.name: "~/";
-    let channelName = message.channel? message.channel.name: "/~";
+    let channelName = message.channel.name? message.channel.name: "/~";
     if (message) {
       console.log('\nUser:『', message.author.username, '』\nGuild & channel:『', guildName, ':', channelName, '』\nMessage: 『', message.content, '』');
     };
